1c9be92260a07adbc3922c5e20b14cf5e3d843a9
Mo-Mans0ur <Moha1998@outlook.dk>
2021-09-10 20:49:00 +0200

    first task done

--- src/MethodProject.java
+++ src/MethodProject.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class MethodProject {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("please input the first number ");
+        double number1 = scanner.nextDouble();
+        System.out.println("please input your second number");
+        double number2 = scanner.nextDouble();
+        System.out.println("please input you third number");
+        double number3 = scanner.nextDouble();
+
+        System.out.println("the lowest number of your input is " + min(number1,number2,number3));
+
+}
+    public static double min(double number1, double number2, double number3) {
+        return Math.min(Math.min(number1,number2),number3);
+
+
 
     }
 }
